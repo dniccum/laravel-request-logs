b631344d6269a4e8e51bdfd5f6d1f1367b117aae
Doug Niccum <doug.niccum@schierproducts.com>
2022-10-14 16:52:51 -0500

    Begins writing unit tests.

---
+++ tests/CleanLogsCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('can remove logs that are older than 3 weeks', function () {
+    $numberOfRecordsToCreate = 40;
+    \Dniccum\LaravelRequestLogs\Models\RequestLog::factory($numberOfRecordsToCreate)->create();
+
+    $date = now()->subDays(config('request-logs.history'));
+    $numberOfRecords = \Dniccum\LaravelRequestLogs\Models\RequestLog::where('created_at', '<', $date)
+        ->count();
+
+    \Pest\Laravel\artisan('request-logs:clean')->assertOk();
+
+    $leftOverRecords = \Dniccum\LaravelRequestLogs\Models\RequestLog::where('created_at', '>', $date)
+        ->count();
+
+    expect($numberOfRecordsToCreate === ($leftOverRecords + $numberOfRecords))->toBeTrue();
+});
